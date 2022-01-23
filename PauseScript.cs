fb8f92fd9a1e2defdff9db659944345c139cf40c
AliFurkanDemir <alifurkan.2001af@gmail.com>
2022-01-23 23:05:04 +0300

    Initial commit

---
+++ PauseScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+
+    bool isPaused = false;
+    // Start is called before the first frame update
+   
+
+    public void pauseGame(){
+        if(isPaused) {
+            Time.timeScale = 1;
+            isPaused = false;
+        }
+
+        else{
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+    }
+
+}
---
+++ Scripts/skorScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class skorScript : MonoBehaviour
+{
+    public static int skorValue = 0;
+    Text skor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        skorValue=0;
+        skor = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        skor.text = "Skor: " + skorValue;
+    }
+}
